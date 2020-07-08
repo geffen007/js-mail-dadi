836cda98d2a900b62435f42d81139b0a642c7ad2
Guido Amodio <guido.amodio@gmail.com>
2020-07-08 15:57:07 +0200

    ci passiamo la giornata

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 var mail = ["mail@mail.it", "posta@posta.com", "piccione@piccione.net"];
-
+console.log(mail);
 
 var tuaMail = prompt ("inserisci la tua mail");
+console.log(tuaMail);
 
 
-
-for (var i = 0; i < mail.lenght; i++) {
+for (var i=0; i<mail.lenght; i++) {
     if (mail[i]==tuaMail){
-        document.getElementById('entra').innerHTML=('adesso puoi entrare');
+        console.log('si')
+    } else {
+        console.log('no')
     }
 }
